e068b9d50a8e285993b9e9ca3041ad6da2d9abfb
Tsai-Ting Yang <u9934007@gmail.com>
2017-08-04 09:37:35 +0800

    Core Data

---
+++ Journal/Journal/ColorAndFontExtension.swift
@@ -0,0 +1,21 @@
+//
+//  ColorAndFontExtension.swift
+//  Journal
+//
+//  Created by 楊采庭 on 2017/8/4.
+//  Copyright © 2017年 楊采庭. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    class func textStyle24Font() -> UIFont {
+        return UIFont.systemFont(ofSize: 20.0, weight: UIFontWeightSemibold)
+    }
+    
+
+
+
+
+}
